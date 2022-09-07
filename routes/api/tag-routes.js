3a25e0ea91e9c25583eb20183f7316874b829ead
Leena Jabr <107494937+LeenaJabr@users.noreply.github.com>
2022-09-06 23:25:19 -0500

    Still an error with MySQL
    an issue with my computer. localhost will not let me reinstall to override forgotten password. will try again

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,83 +1,59 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
-
-router.get('/', async (req, res) => {
-  // find all tags and associated Product data
-  try {
-    const tagData = await Tag.findAll({
-      include: [{ model: Product }, {model: Product, through: ProductTag}],
-    });
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/:id', async (req, res) => {
-  // find a single tag by its `id` and associated Product data
-  try {
-    const tagData = await Tag.findByPk(req.params.id, {
-      include: [{model: Product}, {model: Product, through: ProductTag}],
-    });
-
-    if(!tagData) {
-      res.status(404).json({ message: "No tag found with that id"});
-      return;
-    }
-
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.post('/', async (req, res) => {
-  // create a new tag
-  try {
-    const tagData = await Tag.create(req.body);
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.put('/:id', async (req, res) => {
-  // update a tag's name by its `id` value
-  try {
-    const tagData = await Tag.update(req.body, {
-      where: {
-        id: req.params.id,
+router.get('/', (req, res) => {
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
       },
-    });
-    
-    if (!tagData) {
-      res.status(404).json({ message: "No tag with this id exists"});
-      return;
-    } 
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    ],
+  })
+    .then((tags) => res.status(200).json(tags))
+    .catch((err) => res.status(500).json(err));
 });
 
-router.delete('/:id', async (req, res) => {
-  // delete on tag by its `id` value
-  try {
-    const tagData = await Tag.destroy({
-      where: {
-        id: req.params.id,
+router.get('/:id', (req, res) => {
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
       },
-    });
-    if (!tagData) {
-      res.status(404).json(tagData);
-      return;
-    } 
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    ],
+  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
+});
+
+router.post('/', (req, res) => {
+  Tag.create(req.body)
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
+});
+
+router.put('/:id', (req, res) => {
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
+});
+
+router.delete('/:id', (req, res) => {
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,17 @@
-// import express, roures, and sequelize connection
 const express = require('express');
-const routes = require('./routes');
 const sequelize = require('./config/connection');
 
-
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-sequelize.sync({force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
